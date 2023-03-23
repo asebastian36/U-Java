32ad3aedc1c4ab1af06a4802756b0a19bf441695
asebastian36 <asebastianjob@gmail.com>
2023-03-22 23:46:31 -0600

    avance en el proyecto de MVC

--- ControlClientes/src/main/java/datos/Conexion.java
+++ ControlClientes/src/main/java/datos/Conexion.java
@@ -1,6 +1,10 @@
 package datos;
 
-import javax.activation.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
 
 /**
@@ -13,9 +17,43 @@ public class Conexion {
     private static final String JDBC_USER = "root";
     private static final String JDBC_PASSWORD = "L4c1b0rgv4c4#";
     
+    //  recupera la conexion con la bd
     public static DataSource getDataSource() {
         BasicDataSource dataSource = new BasicDataSource();
         dataSource.setUrl(JDBC_URL);
         dataSource.setUsername(JDBC_USER);
+        dataSource.setPassword(JDBC_PASSWORD);
+        
+        //  establecemos el poll de conexiones se especificaron 50 conexiones
+        dataSource.setInitialSize(50);
+        return dataSource;
+    }
+    
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+    
+    public static void close(ResultSet rs) {
+        try {
+            rs.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(PreparedStatement statement) {
+        try {
+            statement.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
+    
+    public static void close(Connection conn) {
+        try {
+            conn.close();
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 }
